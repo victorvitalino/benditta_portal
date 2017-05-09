d5c5109178685a4fbeca40a13b7c339d7c73c9d3
Victor Vitalino <vitalino.victor@gmail.com>
2017-05-09 16:38:24 -0300

    fix

---
+++ db/migrate/20170509192609_create_dolls.rb
@@ -0,0 +1,14 @@
+class CreateDolls < ActiveRecord::Migration[5.0]
+  def change
+    create_table :dolls do |t|
+      t.string :name
+      t.number :category
+      t.boolean :status
+      t.string :sku
+      t.string :photo
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
