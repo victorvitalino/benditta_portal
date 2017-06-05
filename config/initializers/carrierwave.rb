ccc0822110117bea8ff95b1fac8d3dc757788bfe
Victor Vitalino <vitalino.victor@gmail.com>
2017-06-05 14:43:50 -0300

    fix env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,13 +4,13 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIJ7RXWBCXBRRYOJQ',                        # required
-    aws_secret_access_key: '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',                        # required
-    region:                'sa-east-1',                  # optional, defaults to 'us-east-1'
+    aws_access_key_id:      ENV['S3_KEY'],               # required
+    aws_secret_access_key:  ENV['S3_SECRET'],            # required
+    region:                 ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'benditta'                          # required
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                         # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
