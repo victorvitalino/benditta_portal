3d578d26c063104196967ece9d6868bb1bea27fc
Victor Vitalino <vitalino.victor@gmail.com>
2017-06-19 11:12:41 -0300

    dolls bug

--- app/models/doll.rb
+++ app/models/doll.rb
@@ -1,2 +1,6 @@
 class Doll < ApplicationRecord
+
+
+    enum category: ['Velhas','Mães','Bailarinas','Pretos Velhos','Órixas','Fridas','Especialistas','Fadas','Iconografia de Brasília','Santtas do Sagrado Cotidiano','V.I.Z.I.N.H.O.S','Outras']
+    mount_uploader :photo, PhotoUploader
 end
---
+++ engines/admin/app/controllers/admin/dolls_controller.rb
@@ -0,0 +1,59 @@
+require_dependency 'admin/application_controller'
+
+module Admin
+  class DollsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_doll, only: [:edit, :update, :destroy]
+
+    def index
+      @dolls = Doll.all
+    end
+
+    def new
+      @doll = ::Doll.new
+    end
+
+    def create
+      @doll = ::Doll.new(set_params)
+
+      if @doll.save
+        flash[:success] = t :success
+        redirect_to action: :index
+      else
+        render action: :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @doll.update(set_params)
+        flash[:success] = t :success
+        redirect_to action: :index
+      else
+        render action: :edit
+      end
+    end
+
+    def destroy
+      if @doll.destroy
+        flash[:success] = t :success
+      else
+        flash[:danger] = t :danger
+      end
+
+      redirect_to action: :index
+    end
+
+    private
+
+    def set_params
+      params.require(:doll).permit(:photo,:name, :description, :status, :sku, :category)
+    end
+
+    def set_doll
+      @doll = ::Doll.find(params[:id])
+    end
+  end
+end
