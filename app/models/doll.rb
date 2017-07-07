ba46f6ebe6a5c84741db5c6f0d0e08f7f71cc60b
Victor Vitalino <vitalino.victor@gmail.com>
2017-07-07 16:42:41 -0300

    adicionando uma categoria

--- app/models/doll.rb
+++ app/models/doll.rb
@@ -1,7 +1,7 @@
 class Doll < ApplicationRecord
 
 
-    enum category: ['Velhas','Mães','Bailarinas','Pretos Velhos','Órixas','Fridas','Especialistas','Fadas','Iconografia de Brasília','Santtas do Sagrado Cotidiano','V.I.Z.I.N.H.O.S','Outras']
+    enum category: ['Velhas','Mães','Bailarinas','Pretos Velhos','Órixas','Fridas','Especialistas','Fadas','Iconografia de Brasília','Santtas do Sagrado Cotidiano','V.I.Z.I.N.H.O.S','Turma do Poá','Outras']
     mount_uploader :photo, PhotoUploader
     mount_uploader :photo_2, PhotoUploader
     mount_uploader :photo_3, PhotoUploader
